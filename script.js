b5681c79cdcdec99ca08e55d97d69f2a56ded986
Ernest <zyperis14@gmail.com>
2025-07-16 10:58:10 +0300

    books

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const kingBooksDOM = document.getElementById('king-books');
+const loaderDOM = document.getElementById('loader');
+const tableDOM = document.getElementById('table')
+
+tableDOM.classList.add('hide');
+loaderDOM.classList.add('show');
 
 fetch('https://stephen-king-api.onrender.com/api/books')
     .then((res) => res.json())
     .then((data) => {
-        console.log(data.data);
+        console.log(data);
+
+        tableDOM.classList.remove('hide');
+        loaderDOM.classList.remove('show');
+
         data.data.forEach((book) => {
             kingBooksDOM.insertAdjacentHTML('beforeend',
                 `<tr>
@@ -12,10 +21,18 @@ fetch('https://stephen-king-api.onrender.com/api/books')
                     <td>${book.Publisher}</td>
                     <td>${book.ISBN}</td>
                     <td>${book.Pages}</td>
-                    <td>${book.Notes[0]}</td>
+                    <td>${book.Notes[0] ? book.Notes.join(' ') : 'No notes'}</td>
                 </tr>`
             )
         })
     })
     .catch((error) => console.log(error));
 
+kingBooksDOM.addEventListener('click', (e) => {
+    const bid = e.target.parentElement.dataset.bookid;
+    console.log(bid);
+    fetch('https://stephen-king-api.onrender.com/api/book/' + bid)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .then(err => console.log(err));
+});
